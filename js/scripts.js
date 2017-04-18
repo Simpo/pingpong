e3ac8d17e92b3f80ad5d3aeaaefa4eef23e05a67
Simpo <marcusgathii2011@gmail.com>
2017-04-18 18:49:58 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
-$(document).ready(function () {
-    for (var i=1; i <= 20; i++)
-    {
-        if (#i % 15 == 0)
-            console.log("FizzBuzz");
-        else if (#i % 3 == 0)
-            console.log("Fizz");
-        else if (#i % 5 == 0)
-            console.log("Buzz");
-        else
-            console.log(i);
-       }
-    });
+$(document).ready(function(){
+  $("form#formInput").submit(function(){
+    event.preventDefault();
+    var userNumber = parseInt($(input#userInput).val());
+    var results = [];
+
+    for (var i = 1; i <= userNumber; i++) {
+        if (i % 15 === 0){
+         results.push("PingPong");
+        }
+        else if (i % 3 === 0){
+          results.push("Ping");
+        }
+
+        else if (i % 5 === 0){
+          results.push("Pong");
+        }
+
+        else{
+          results.push(i);
+        }
+      }
+
+      $(div#resultz).append(<li> + results + </li>)
+  });
+});
